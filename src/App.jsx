9571b8b6bd44b7218d4a6b067db9b62ba2d79935
Oluwatomiwa Oluwagbemiro <oluwatomiwasam@gmail.com>
2023-06-27 04:45:33 +0200

    Add pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
 
-function App() {
-  const [count, setCount] = useState(0)
+import Account from './components/Account';
+import Home from './components/Home';
+import LinkRedirect from './components/LinkRedirect';
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+const App = () => {
+  const [user, setUser] = useState(null);
 
-export default App
+  setUser(null);
+
+  <Routes>
+    <Route exact path="/">
+      {user ? <Navigate to="/account" /> : <Home />}
+    </Route>
+    <Route path="/account">{user ? <Account /> : <Navigate to="/" />}</Route>
+    <Route path="/:shortCode">
+      <LinkRedirect />
+    </Route>
+  </Routes>;
+};
+
+export default App;
---
+++ src/components/LinkRedirect/index.jsx
@@ -0,0 +1,5 @@
+function LinkRedirect() {
+  return <div>LinkRedirect</div>;
+}
+
+export default LinkRedirect;
