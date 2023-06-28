c663df127c6113eb315a27fdd823feac2636ee49
Oluwatomiwa Oluwagbemiro <oluwatomiwasam@gmail.com>
2023-06-29 00:13:20 +0200

    add list with demo data

---
+++ src/components/Account/Card.jsx
@@ -0,0 +1,68 @@
+/* eslint-disable react/prop-types */
+import { Box, Button, Hidden, Typography } from "@material-ui/core";
+import { BarChart as ChartIcon } from "@material-ui/icons";
+import format from "date-fns/format";
+
+const Card = ({
+  id,
+  createdAt,
+  name,
+  longURL,
+  shortCode,
+  totalClicks,
+  deleteLink,
+  copyLink,
+}) => {
+  const shortUrl = `${window.location.host}/${shortCode}`;
+  return (
+    <Box display="flex" justifyContent="space-between" alignItems="center">
+      <Box width="50%">
+        <Typography color="textSecondary" variant="overline">
+          Created at {format(createdAt, "d MMM, HH:mm")}
+        </Typography>
+        <Box my={2}>
+          <Typography style={{ marginBottom: "5px" }} variant="h5">
+            {name}
+          </Typography>
+          <Typography style={{ overflow: "hidden", textOverflow: "ellipsis" }}>
+            {longURL}
+          </Typography>
+        </Box>
+        <Box display="flex" alignItems="center">
+          <Typography color="primary">{shortUrl}</Typography>
+          <Box mx={2}>
+            <Button
+              onClick={() => copyLink(shortUrl)}
+              color="primary"
+              size="small"
+              variant="outlined"
+            >
+              Copy
+            </Button>
+          </Box>
+          <Button
+            onClick={() => deleteLink(id)}
+            color="secondary"
+            size="small"
+            variant="contained"
+            disableElevation
+          >
+            Delete
+          </Button>
+        </Box>
+      </Box>
+
+      <Box>
+        <Box display="flex" justifyContent="center">
+          <Typography>{totalClicks}</Typography>
+          <ChartIcon />
+        </Box>
+        <Hidden only="xs">
+          <Typography variant="overline">Total Clicks</Typography>
+        </Hidden>
+      </Box>
+    </Box>
+  );
+};
+
+export default Card;
--- src/components/Account/index.jsx
+++ src/components/Account/index.jsx
@@ -1,7 +1,36 @@
-import { Box, Button, Grid, Typography } from "@material-ui/core";
-import { useState } from "react";
+import { Box, Button, Divider, Grid, Typography } from "@material-ui/core";
+import { Fragment, useState } from "react";
 
+import Card from "./Card";
 import Navbar from "./Navbar";
+
+const dummyData = [
+  {
+    id: "31r08ms0fam",
+    createdAt: new Date(),
+    name: "My website",
+    longURL: "https://google.com",
+    shortCode: "masdo",
+    totalClicks: 313,
+  },
+  {
+    id: "31r08asdasfam",
+    createdAt: new Date(),
+    name: "E-book",
+    longURL: "https://drive.google.com/asdokasnd89",
+    shortCode: "182as",
+    totalClicks: 32,
+  },
+  {
+    id: "asdasdas",
+    createdAt: new Date(),
+    name: "E-book",
+    longURL: "https://drive.google.com/asdokasnd89",
+    shortCode: "182as",
+    totalClicks: 32,
+    cool: ["1,2,3"],
+  },
+];
 
 function Account() {
   const [openModal, setOpenModal] = useState(false);
@@ -25,6 +54,17 @@ function Account() {
                 Create new
               </Button>
             </Box>
+
+            {dummyData.map((link, idx) => (
+              <Fragment key={link.id}>
+                <Card {...link} deleteLink={() => {}} copyLink={() => {}} />
+                {idx !== dummyData.length - 1 && (
+                  <Box my={4}>
+                    <Divider />
+                  </Box>
+                )}
+              </Fragment>
+            ))}
           </Grid>
         </Grid>
       </Box>
