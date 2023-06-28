41f56373cc6cf1ff7d81c05ea55f3bbbdc50c193
Oluwatomiwa Oluwagbemiro <oluwatomiwasam@gmail.com>
2023-06-29 00:25:03 +0200

    remove default css and add Theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
+import { ThemeProvider } from "@material-ui/core";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Account from "./components/Account";
 import Home from "./components/Home";
 import LinkRedirect from "./components/LinkRedirect";
+import theme from "./theme";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="account" element={<Account />} />
-        <Route path=":shortCode" element={<LinkRedirect />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="account" element={<Account />} />
+          <Route path=":shortCode" element={<LinkRedirect />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 };
 
