651b20e1d56bed3f741488b3a8fd9a2f198ae8b2
Oluwatomiwa Oluwagbemiro <oluwatomiwasam@gmail.com>
2023-07-03 06:22:08 +0200

    implement signout

--- src/components/Account/Navbar.jsx
+++ src/components/Account/Navbar.jsx
@@ -1,5 +1,6 @@
-import { AppBar, Box, Button, Toolbar, Typography } from "@material-ui/core";
-
+import { AppBar, Box, Button, Toolbar, Typography } from '@material-ui/core';
+import { signOut } from 'firebase/auth';
+import { auth } from "../../firebase";
 const Navbar = () => {
   return (
     <AppBar elevation={0} color="secondary" position="static">
@@ -7,7 +8,9 @@ const Navbar = () => {
         <Typography variant="h6">Scissor</Typography>
         <Box ml="auto">
           <Button color="inherit">Links</Button>
-          <Button color="inherit">Logout</Button>
+          <Button color="inherit" onClick={() => signOut(auth)}>
+            Logout
+          </Button>
         </Box>
       </Toolbar>
     </AppBar>
